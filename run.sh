0e2df2394d6cf4d08f0e2047cfe8826c7fcf04ba
Hiron Das <hcdas.09@gmail.com>
2026-03-03 17:17:06 +0600

    add exit 0 to stop after a task run

--- run.sh
+++ run.sh
@@ -16,21 +16,4 @@ fi
 echo "Starting daemon (Ephemeral Mode)..."
 act_runner daemon --once
 
-# 3. Shutdown Logic
-# Once the runner exits, we tell Railway to stop this service instance.
-if [ -n "$RAILWAY_TOKEN" ]; then
-  echo "Job finished. Signaling Railway to stop service..."
-  
-  # We use a curl command to the Railway GraphQL API to 'down' the service
-  # This is more reliable than the CLI inside a container
-  curl --request POST \
-    --url https://backboard.railway.app/graphql/v2 \
-    --header "Project-Access-Toke: $RAILWAY_TOKEN" \
-    --header "Content-Type: application/json" \
-    --data "{
-      \"query\": \"mutation serviceInstanceStop(\$serviceId: String!) { serviceInstanceStop(serviceId: \$serviceId) }\",
-      \"variables\": { \"serviceId\": \"$RAILWAY_SERVICE_ID\" }
-    }"
-else
-  echo "RAILWAY_TOKEN not found. Manual shutdown required or service will restart."
-fi
+exit 0
